ed91f92fac4420d3286ce48130be4a07816d66f4
ZahangirAlamJP <thezahangir@gmail.com>
2022-02-07 20:22:09 +0600

    DefaultTabController // 26.2

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
   runApp(MaterialApp(
-    title: 'Sweet App',
+    theme: ThemeData(primaryColor: Colors.amber),
+    debugShowCheckedModeBanner: false,
     home: MyApp(),
+    title: 'DefaultTabController',
   ));
 }
 
@@ -18,62 +19,41 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Dialog box'),
-      ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            AlertDialog(
-              title: Text('Delete File'),
-              content: Text('Are You Confirm'),
-              actions: [
-                TextButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    child: Text('No')),
-                TextButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    child: Text('Yes')),
-              ],
-            ),
-            ///// IOS ///
-            CupertinoAlertDialog(
-              title: Text('Delete File'),
-              content: Text('Are You Confirm'),
-              actions: [
-                CupertinoDialogAction(child: Text('No')),
-                CupertinoDialogAction(child: Text('Yes')),
-              ],
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  showDialog(
-                      context: context,
-                      builder: (context) => AlertDialog(
-                            title: Text('Delete File'),
-                            content: Text('Are You Confirm'),
-                            actions: [
-                              TextButton(
-                                  onPressed: () {
-                                    Navigator.pop(context);
-                                  },
-                                  child: Text('No')),
-                              TextButton(
-                                  onPressed: () {
-                                    Navigator.pop(context);
-                                  },
-                                  child: Text('Yes')),
-                            ],
-                          ));
-                },
-                child: Text('Exit Now!')),
+    return DefaultTabController(
+      length: 5,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('DefaultTabController'),
+          centerTitle: true,
+          leading: IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
+          actions: [
+            IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+            IconButton(onPressed: () {}, icon: Icon(Icons.settings)),
           ],
+          bottom: TabBar(tabs: [
+            Tab(
+              icon: Icon(Icons.dashboard),
+              child: Text('Dashboard'),
+            ),
+            Tab(
+              icon: Icon(Icons.games),
+              child: Text('Game'),
+            ),
+            Tab(
+              icon: Icon(Icons.hardware),
+              child: Text('Hardware'),
+            ),
+            Tab(
+              icon: Icon(Icons.add_chart),
+              child: Text('Chart'),
+            ),
+            Tab(
+              icon: Icon(Icons.face),
+              child: Text('Profile'),
+            ),
+          ]),
         ),
+        body: Center(),
       ),
     );
   }
