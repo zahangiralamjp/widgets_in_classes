8599dd5fb56a74af821dd605fa9d8c0ffc30c831
ZahangirAlamJP <thezahangir@gmail.com>
2022-02-07 20:26:15 +0600

    MyCheckBox // 31.2

--- lib/main.dart
+++ lib/main.dart
@@ -3,93 +3,51 @@ import 'package:flutter/material.dart';
 void main(List<String> args) {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: MyRadio(),
+    home: MyCheckBox(),
   ));
 }
 
-class MyRadio extends StatefulWidget {
-  const MyRadio({Key? key}) : super(key: key);
+class MyCheckBox extends StatefulWidget {
+  const MyCheckBox({Key? key}) : super(key: key);
 
   @override
-  _MyRadioState createState() => _MyRadioState();
+  _MyCheckBoxState createState() => _MyCheckBoxState();
 }
 
-class _MyRadioState extends State<MyRadio> {
-  int _value = 1;
+class _MyCheckBoxState extends State<MyCheckBox> {
+  bool value = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Radio'),
-        centerTitle: true,
+        title: Text('Checkbox'),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
+      body: ListView(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(32.0),
-            child: Row(
-              children: [
-                Radio(
-                  value: 1,
-                  groupValue: _value,
-                  onChanged: (value) {
-                    setState(() {
-                      _value = 1;
-                    });
-                  },
-                ),
-                Text('Radio 1'),
-                SizedBox(
-                  height: 10,
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(32.0),
-            child: Row(
-              children: [
-                Radio(
-                  value: 2,
-                  groupValue: _value,
-                  onChanged: (value) {
-                    setState(() {
-                      _value = 2;
-                    });
-                  },
-                ),
-                Text('Radio 2'),
-                SizedBox(
-                  height: 10,
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(32.0),
-            child: Row(
-              children: [
-                Radio(
-                  value: 3,
-                  groupValue: _value,
-                  onChanged: (value) {
-                    setState(() {
-                      _value = 3;
-                    });
-                  },
-                ),
-                Text('Radio 3'),
-                SizedBox(
-                  height: 10,
-                ),
-              ],
-            ),
-          ),
+          buildCheckbox(),
         ],
       ),
     );
   }
+
+  Widget buildCheckbox() => ListTile(
+        onTap: () {
+          setState(() {
+            this.value = !value;
+          });
+        },
+        leading: Checkbox(
+          value: value,
+          onChanged: (value) {
+            setState(() {
+              this.value = value!;
+            });
+          },
+        ),
+        title: Text(
+          'Checkbox 1',
+          style: TextStyle(fontSize: 20),
+        ),
+      );
 }
