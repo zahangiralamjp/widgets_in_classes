b08c512dee2ba12da5242ecfece0f492e4514411
ZahangirAlamJP <thezahangir@gmail.com>
2022-02-07 20:21:37 +0600

    Alert Dialog

--- lib/main.dart
+++ lib/main.dart
@@ -1,112 +1,78 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
   runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: MySignUpForm(),
+    title: 'Sweet App',
+    home: MyApp(),
   ));
 }
 
-class MySignUpForm extends StatefulWidget {
-  const MySignUpForm({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
-  _MySignUpFormState createState() => _MySignUpFormState();
+  _MyAppState createState() => _MyAppState();
 }
 
-class _MySignUpFormState extends State<MySignUpForm> {
-  GlobalKey<FormState> formkey = GlobalKey<FormState>();
-
-  void vaildate() {
-    if (formkey.currentState!.validate()) {
-      print('Validated');
-    } else {
-      print('Not Validated');
-    }
-  }
-
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('My Sign Up Form'),
-        centerTitle: true,
+        title: Text('Dialog box'),
       ),
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: SingleChildScrollView(
-            scrollDirection: Axis.vertical,
-            child: Form(
-                key: formkey,
-                child: Column(
-                  children: [
-                    TextFormField(
-                      keyboardType: TextInputType.text,
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'User Email',
-                          labelStyle:
-                              TextStyle(fontSize: 20, color: Colors.amber),
-                          hintText: 'Your Email'),
-                      validator: (YourEmail) {
-                        if (YourEmail!.isEmpty) {
-                          return 'Your Email Empty';
-                        }
-                        return null;
-                      },
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    TextFormField(
-                      keyboardType: TextInputType.visiblePassword,
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Password',
-                          labelStyle:
-                              TextStyle(fontSize: 20, color: Colors.blue),
-                          hintText: 'Your Password'),
-                      validator: (password) {
-                        if (password!.isEmpty) {
-                          return 'Your password Empty';
-                        }
-                        return null;
-                      },
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    TextFormField(
-                      keyboardType: TextInputType.phone,
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'PhoneNumber',
-                          labelStyle:
-                              TextStyle(fontSize: 20, color: Colors.green),
-                          hintText: 'Your Phone Number'),
-                      validator: (PhoneNumber) {
-                        if (PhoneNumber!.isEmpty) {
-                          return 'User Name Empty';
-                        }
-                        return null;
-                      },
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 245.0, vertical: 16.0),
-                        ),
-                        onPressed: () {
-                          vaildate();
-                        },
-                        child: Text('Sign Up')),
-                  ],
-                )),
-          ),
+        child: Column(
+          children: <Widget>[
+            AlertDialog(
+              title: Text('Delete File'),
+              content: Text('Are You Confirm'),
+              actions: [
+                TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text('No')),
+                TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text('Yes')),
+              ],
+            ),
+            ///// IOS ///
+            CupertinoAlertDialog(
+              title: Text('Delete File'),
+              content: Text('Are You Confirm'),
+              actions: [
+                CupertinoDialogAction(child: Text('No')),
+                CupertinoDialogAction(child: Text('Yes')),
+              ],
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  showDialog(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                            title: Text('Delete File'),
+                            content: Text('Are You Confirm'),
+                            actions: [
+                              TextButton(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                  child: Text('No')),
+                              TextButton(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                  child: Text('Yes')),
+                            ],
+                          ));
+                },
+                child: Text('Exit Now!')),
+          ],
         ),
       ),
     );
