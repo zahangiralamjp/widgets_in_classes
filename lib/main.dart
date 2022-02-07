bd8e1111b5346e41fbe44d65d5b6eb09cc2b9b8f
ZahangirAlamJP <thezahangir@gmail.com>
2022-02-07 20:14:28 +0600

    ElevatedButton

--- lib/main.dart
+++ lib/main.dart
@@ -2,42 +2,29 @@ import 'package:flutter/material.dart';
 
 void main(List<String> args) {
   runApp(MaterialApp(
-    theme: ThemeData(primarySwatch: Colors.brown),
+    theme: ThemeData(primarySwatch: Colors.pink),
+    debugShowCheckedModeBanner: false,
     home: MyApp(),
   ));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Text Field'),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'User Name',
-                  hintText: 'Enter User Name',
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Password',
-                  hintText: 'Enter password',
-                ),
-                keyboardType: TextInputType.emailAddress,
-              ),
-            ],
-          ),
-        ));
+      appBar: AppBar(
+        title: Text('Blue Bar'),
+      ),
+      body: Center(
+        child: ElevatedButton(onPressed: () {}, child: Text('click here')),
+      ),
+    );
   }
 }
